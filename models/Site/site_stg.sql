9cf5439440a2ecc124b866720620327f0778e34e
satyabrata.mishra@manyavar.com <satyabrata.mishra@manyavar.com>
2025-09-09 13:00:40 +0000

    1 code done

---
+++ models/Site/site_stg.sql
@@ -0,0 +1,21 @@
+{{ config(
+    materialized='table',
+    schema='ANALYTICS'
+) }}
+
+
+SELECT
+    CMPCODE,
+    CNNAME,
+    CODE,
+    CTNAME,
+    CUS_DISTANCE,
+    CUS_GROUP,
+    DIST,
+    EXT,
+    ISD,
+    STD,
+    STNAME,
+    ZONE
+--FROM vflpoc.raw.admcity;
+from {{ source('raw', 'admcity') }}
