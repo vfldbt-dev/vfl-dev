c0d78245f53455ddc7b859cc84a09537bfa70b66
Satyabrata Mishra <satyabrata.mishra@manyavar.com>
2025-09-17 10:25:02 +0000

    joining both

---
+++ models/Site/join_city_site.sql
@@ -0,0 +1,15 @@
+{{config (materialized='table')}}
+
+SELECT 
+s.SLCODE as Site_Code,
+s.SHRTNAME as Store_Name,
+s.CTNAME as Store_City,
+s.ADDRESS as Store_Address,
+
+c.ZONE as Zone,
+c.class as Tier
+
+from {{ref("site_stg")}} s
+join {{ref("site2_stg")}} c
+on s.CMPCODE=c.CMPCODE
+and s.CODE=c.CODE
