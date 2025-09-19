b30f587a75d9af2eb27048b6bcf18a14c1221091
Satyabrata Mishra <satyabrata.mishra@manyavar.com>
2025-09-19 05:57:49 +0000

    ch

--- models/Site/vw_join_city_site.sql
+++ models/Site/vw_join_city_site.sql
@@ -1,13 +1,16 @@
 {{ config(materialized='view') }}
 
 
-SELECT 
+SELECT
+s.*,c.*
+/* 
     s.SLCODE as Site_Code,
     s.SHRTNAME as Store_Name,
     s.CTNAME as Store_City,
     s.ADDRESS as Store_Address,
     c.ZONE as Zone,
     --c."CLASS" as Tier
+*/
 FROM {{ ref("vw_site_stg") }} c
 right JOIN {{ ref("vw_site2_stg") }} s
     ON s.CTNAME = c.CTNAME
