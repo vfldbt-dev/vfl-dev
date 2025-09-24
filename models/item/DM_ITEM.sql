18b7c796b19e3f84a453059d4474efe1bef64810
Satyabrata Mishra <satyabrata.mishra@manyavar.com>
2025-09-24 04:56:35 +0000

    ch

--- models/item/DM_ITEM.sql
+++ models/item/DM_ITEM.sql
@@ -1,4 +1,13 @@
 {{ config(materialized='view') }}
 
+/*
+{{ config(
+    materialized='incremental',
+    incremental_strategy='append',
+    database='demo_db',
+    schema='public'
+) }}
+*/
+
 SELECT * FROM 
 {{ source('raw', 'INVITEM') }}
