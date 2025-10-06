e81dbdf93c1b31dd27b4f92a77de91a3482f6043
Satyabrata Mishra <satyabrata.mishra@manyavar.com>
2025-10-06 10:25:21 +0000

    ch

--- macros/apply_aliases.sql
+++ macros/apply_aliases.sql
@@ -1,13 +1,7 @@
 {% macro apply_aliases(model_name) %}
-    {% set column_map = var('column_alias_map', {}) %}
-    {% if column_map.get(model_name) is none %}
-        {% do exceptions.raise_compiler_error("No column_alias_map found for model: " ~ model_name) %}
-    {% endif %}
-    
-    {% set alias_map = column_map[model_name] %}
-
-    {%- for col, alias in alias_map.items() %}
+    {% set alias_map = var('column_alias_map')[model_name] %}
+    {% for col, alias in alias_map.items() %}
         {{ col }} AS "{{ alias }}"
         {%- if not loop.last %}, {% endif %}
-    {%- endfor %}
+    {% endfor %}
 {% endmacro %}
