657842e70b927ab788b8692ee34b1944f4f8ee76
Satyabrata Mishra <satyabrata.mishra@manyavar.com>
2025-10-06 08:34:25 +0000

    ch

--- macros/select_with_alias.sql
+++
@@ -1,5 +0,0 @@
-{% macro select_with_alias(table_alias, rename_map) %}
-    {%- for col, alias in rename_map.items() -%}
-        {{ table_alias }}.{{ col }} AS {{ alias }}{% if not loop.last %}, {% endif %}
-    {%- endfor -%}
-{% endmacro %}
