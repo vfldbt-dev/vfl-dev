1f4f8bcb1b0693424fdf38db261d9739eb2a304f
Satyabrata Mishra <satyabrata.mishra@manyavar.com>
2025-10-29 11:42:11 +0000

    ch

--- macros/apply_governance_tags.sql
+++ macros/apply_governance_tags.sql
@@ -1,15 +1,18 @@
-{% macro apply_governance_tags(model) %}
-  {% set meta = model.meta if model.meta is defined else {} %}
+{% macro apply_governance_tags() %}
+    {% set relation = this %}
+    {% set materialization = config.get('materialized', default='view') %}
+    {% set alter_cmd = 'ALTER VIEW' if materialization == 'view' else 'ALTER TABLE' %}
 
-  {% if meta.get('steward') %}
-    ALTER VIEW {{ this }} SET TAG steward = '{{ meta.get("steward") }}';
-  {% endif %}
-  
-  {% if meta.get('approver') %}
-    ALTER VIEW {{ this }} SET TAG approver = '{{ meta.get("approver") }}';
-  {% endif %}
-  
-  {% if meta.get('support') %}
-    ALTER VIEW {{ this }} SET TAG support = '{{ meta.get("support") }}';
-  {% endif %}
+    {% if execute %}
+        {% set model_meta = graph.nodes[model.unique_id].meta %}
+        {% if model_meta.get('steward') %}
+            {% do run_query(alter_cmd ~ ' ' ~ relation ~ ' SET TAG steward = \'' ~ model_meta.get("steward") ~ '\'') %}
+        {% endif %}
+        {% if model_meta.get('approver') %}
+            {% do run_query(alter_cmd ~ ' ' ~ relation ~ ' SET TAG approver = \'' ~ model_meta.get("approver") ~ '\'') %}
+        {% endif %}
+        {% if model_meta.get('support') %}
+            {% do run_query(alter_cmd ~ ' ' ~ relation ~ ' SET TAG support = \'' ~ model_meta.get("support") ~ '\'') %}
+        {% endif %}
+    {% endif %}
 {% endmacro %}
